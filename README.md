e13687979036b24ddff38a308f0c39ad0e383b14
Anthony Onabanjo <deflexable@gmail.com>
2023-06-27 23:40:56 +0100

    fixing stuffs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mosquitodb
 
-MosquitoDB is a NoSQL database engine adapted with super fast, memory efficient, secured and fully queried features
+Under the hood, mosquitodb uses Mongodb to store it data and [express](https://www.npmjs.com/package/express), [socket.io](https://www.npmjs.com/package/socket.io) for making request, so make sure you have [mongodb](https://www.mongodb.com/docs/manual/installation/) installed before using this package
 
 ## Installation
 
@@ -24,22 +24,56 @@ import MosquitoDbServer from "mosquitodb";
 const heavenyaApp = new MosquitoDbServer({
   projectName: 'heavenya',
   port: 4534, // default to 4291
-  signerKey: 'random_hash_key_for_signing_auth_requests', // must be 90 length
+  signerKey: 'random_90_hash_key_for_signing_jwt_tokens', // must be 90 length
   accessKey: 'this_is_my_private_password', // keep this private if you don't provide databaseRules or storageRules
   databaseRules: ({ auth, collection, value, afterData, beforeData, operation, ...otherProps })=> new Promise((resolve, reject)=> {
+    // validate and authorize all incoming request to the database
     if (collection === 'user') {
         if (afterData && auth && auth.uid === value._id) {
             resolve(); // allow read/write
-        } else reject(); // reject read/write
-    } // blah, blah, other algorithm
+        } else reject('You don`t own this data, stay away'); // reject read/write
+    } else if (collection === 'other_paths') {
+      // blah, blah, other algorithm ...
+    }
   }),
   storageRules: ({...props})=> new Promise(resolve=> {
+    // validate and authorize all uploads/downloads
     resolve(true) // handle read/write yourself here
-  })
+  }),
+  googleAuthConfig: {
+    clientID: 'your_google_authentication_clientID.apps.googleusercontent.com'
+  },
+  appleAuthConfig: {
+    ...props
+  },
+  ...otherProps
 });
 ```
 
-your server is ready to be deploy on a node.js environment 🚀
+your server is ready to be deploy on a node.js environment! 🚀. Now install any mosquitodb client sdk and start making requests to the server.
+
+### SDKs
+- [react-native-mosquitodb](https://github.com/deflexable/react-native-mosquitodb) for react native apps
+- [mosquitodb-web](https://github.com/deflexable/mosquitodb-web) for web platform
+
+## Additional Documentations
+- [Logging Level](#logging-levels)
+- [Database Rules](#database-rules)
+- [Storage Rules](#storage-rules)
+- [Authentication](#authentication)
+   - [Merge Auth Account](#google-auth-setup)
+   - [Google Auth Setup](#google-auth-setup)
+   - [Apple Auth Setup](#apple-auth-setup)
+   - [Facebook Auth Setup](#facebook-auth-setup)
+   - [Twitter Auth Setup](#twitter-auth-setup)
+   - [Github Auth Setup](#google-auth-setup)
+   - [Fallback Auth Setup](#fallback-auth-setup)
+   - [Google Auth Setup](#google-auth-setup)
+
+
+<!-- ## Platform using Mosquitodb in production
+- [Heavenya - christian events]()
+- [Inspire - christian audio]() -->
 
 ## Contributing
 
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 import { Auth, Db, Document, UpdateDescription } from "mongodb";
 import express from "express";
+import { CorsOptions } from "cors";
 
 interface SimpleError {
 
@@ -13,7 +14,7 @@ interface DatabaseRulesSnapshot {
 
 }
 
-type LogLevel = 'all' | 'disabled' | 'auth' | 'database' | 'storage' | 'outside-requests' | 'content';
+type LogLevel = 'all' | 'disabled' | 'auth' | 'database' | 'storage' | 'external-requests' | 'served-content' | 'database-snapshot';
 
 interface GoogleAuthConfig {
     clientID?: string;
@@ -39,25 +40,123 @@ interface FallbackAuthConfig {
 
 }
 
+interface SneakSignupAuthConfig {
+    email?: string;
+    password?: string;
+    name?: string;
+    metadata: Object
+    token?: string;
+    request: express.Request;
+    method: 'custom' | 'google' | 'apple' | 'github' | 'twitter' | 'facebook';
+}
+
+interface StaticContentProps {
+    /**
+     * Enable or disable accepting ranged requests, defaults to true.
+     * Disabling this will not send Accept-Ranges and ignore the contents of the Range request header.
+     */
+    acceptRanges?: boolean | undefined;
+
+    /**
+     * Enable or disable setting Cache-Control response header, defaults to true.
+     * Disabling this will ignore the maxAge option.
+     */
+    cacheControl?: boolean | undefined;
+
+    /**
+     * Set how "dotfiles" are treated when encountered.
+     * A dotfile is a file or directory that begins with a dot (".").
+     * Note this check is done on the path itself without checking if the path actually exists on the disk.
+     * If root is specified, only the dotfiles above the root are checked (i.e. the root itself can be within a dotfile when when set to "deny").
+     * 'allow' No special treatment for dotfiles.
+     * 'deny' Send a 403 for any request for a dotfile.
+     * 'ignore' Pretend like the dotfile does not exist and 404.
+     * The default value is similar to 'ignore', with the exception that this default will not ignore the files within a directory that begins with a dot, for backward-compatibility.
+     */
+    dotfiles?: "allow" | "deny" | "ignore" | undefined;
+
+    /**
+     * Byte offset at which the stream ends, defaults to the length of the file minus 1.
+     * The end is inclusive in the stream, meaning end: 3 will include the 4th byte in the stream.
+     */
+    end?: number | undefined;
+
+    /**
+     * Enable or disable etag generation, defaults to true.
+     */
+    etag?: boolean | undefined;
+
+    /**
+     * If a given file doesn't exist, try appending one of the given extensions, in the given order.
+     * By default, this is disabled (set to false).
+     * An example value that will serve extension-less HTML files: ['html', 'htm'].
+     * This is skipped if the requested file already has an extension.
+     */
+    extensions?: string[] | string | boolean | undefined;
+
+    /**
+     * Enable or disable the immutable directive in the Cache-Control response header, defaults to false.
+     * If set to true, the maxAge option should also be specified to enable caching.
+     * The immutable directive will prevent supported clients from making conditional requests during the life of the maxAge option to check if the file has changed.
+     * @default false
+     */
+    immutable?: boolean | undefined;
+
+    /**
+     * By default send supports "index.html" files, to disable this set false or to supply a new index pass a string or an array in preferred order.
+     */
+    index?: string[] | string | boolean | undefined;
+
+    /**
+     * Enable or disable Last-Modified header, defaults to true.
+     * Uses the file system's last modified value.
+     */
+    lastModified?: boolean | undefined;
+
+    /**
+     * Provide a max-age in milliseconds for http caching, defaults to 0.
+     * This can also be a string accepted by the ms module.
+     */
+    maxAge?: string | number | undefined;
+
+    /**
+     * Serve files relative to path.
+     */
+    root?: string | undefined;
+
+    /**
+     * Byte offset at which the stream starts, defaults to 0.
+     * The start is inclusive, meaning start: 2 will include the 3rd byte in the stream.
+     */
+    start?: number | undefined;
+}
+
 interface MosquitoDbServerConfig {
-    projectName: string,
-    signerKey: string,
-    storageRules: (snapshot?: StorageRulesSnapshot) => Promise<void>,
-    databaseRules: (snapshot?: DatabaseRulesSnapshot) => Promise<void>,
-    port?: number,
-    enableSequentialUid?: boolean,
-    accessKey: string,
-    disableCrossLogin?: boolean,
-    logger?: LogLevel | LogLevel[],
-    dbUrl?: string,
-    dbName?: string,
-    mergeAuthAccount?: boolean,
-    googleAuthConfig?: GoogleAuthConfig,
-    appleAuthConfig?: AppleAuthConfig,
-    facebookAuthConfig?: FacebookAuthConfig,
-    githubAuthConfig?: GithubAuthConfig,
-    twitterAuthConfig?: TwitterAuthConfig,
-    fallbackAuthConfig?: FallbackAuthConfig
+    projectName: string;
+    signerKey: string;
+    storageRules: (snapshot?: StorageRulesSnapshot) => Promise<void>;
+    databaseRules: (snapshot?: DatabaseRulesSnapshot) => Promise<void>;
+    port?: number;
+    enableSequentialUid?: boolean;
+    accessKey: string;
+    disableCrossLogin?: boolean;
+    logger?: LogLevel | LogLevel[];
+    externalAddress?: string;
+    hostname?: string;
+    dbUrl?: string;
+    dbName?: string;
+    mergeAuthAccount?: boolean;
+    sneakSignupAuth?: (config: SneakSignupAuthConfig) => void;
+    googleAuthConfig?: GoogleAuthConfig;
+    appleAuthConfig?: AppleAuthConfig;
+    facebookAuthConfig?: FacebookAuthConfig;
+    githubAuthConfig?: GithubAuthConfig;
+    twitterAuthConfig?: TwitterAuthConfig;
+    fallbackAuthConfig?: FallbackAuthConfig;
+    staticContentProps?: StaticContentProps;
+    staticContentMaxAge?: number;
+    staticContentCacheControl?: number;
+    corsOrigin?: CorsOptions;
 }
 
 interface UserProfile {
@@ -79,6 +178,10 @@ interface AuthData {
     emailVerified: boolean;
     profile: UserProfile;
     disabled: boolean;
+}
+
+interface UserData extends AuthData {
+    password?: string;
 }
 
 interface JWTAuthData extends AuthData {
@@ -133,6 +236,9 @@ export default class MosquitoDbServer {
     constructor(config: MosquitoDbServerConfig);
 
     getDatabase(dbName?: string, dbUrl?: string): Db;
+    verifyToken(token: string): Promise<AuthData>;
+    validateToken(token: string): Promise<AuthData>;
+    invalidateToken(token: string): Promise<void | boolean>;
     listenHttpsRequest(route: string, callback?: (request: express.Request, response: express.Response, auth?: JWTAuthData | null) => void, options?: MosquitoDbHttpOptions): void;
     listenDatabase(collection: string, callback?: (data: DatabaseListenerCallbackData) => void, options?: DatabaseListenerOption): void;
     listenStorage(collection: string, callback?: () => void): void;
@@ -147,6 +253,6 @@ export default class MosquitoDbServer {
     updateUserPassword(uid: string, password: string): Promise<void>;
     updateUserEmailVerify(uid: string, verified: boolean): Promise<void>;
     disableUser(uid: string, disable: boolean): Promise<void>;
+    getUserData(uid: string): Promise<UserData>;
+    linkToFile(link: string): string;
 }
-
-export function STORAGE_URL_TO_FILE(url: string, projectName: string): string;
