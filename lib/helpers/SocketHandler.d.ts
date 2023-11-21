4d29c1df3826d7cbb593285173a7a387c51b8f3a
Anthony Onabanjo <deflexable@gmail.com>
2023-11-21 12:37:48 +0100

    fixing stuffs

--- lib/helpers/SocketHandler.d.ts
+++ lib/helpers/SocketHandler.d.ts
@@ -3,4 +3,4 @@ import { Socket } from "socket.io";
 
 // export const handleSocketPlug: Server['on'];
 
-export function handleSocketPlug(path: string, callback: (socket: Socket, response: any, roof?: any) => void): (socket: Socket) => void;
+export function handleSocketPlug(path: string, callback: (socket: Socket, roof?: any, ...response: any) => void): (socket: Socket) => void;
